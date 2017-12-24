471a9f7c28ee6bcb58f34e12b9791c5faf97592a
skw321 <skwuslife@gmail.com>
2017-12-24 15:22:20 -0600

    2017/12/24

---
+++ src/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {
+
+        if(head == null){
+            return false;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while(fast != slow){
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
